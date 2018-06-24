8bf0f459f549fec045af4bc3450712a460c0185d
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2018-06-24 14:35:10 -0400

    6-24-18 commit 2

---
+++ client/src/pages/game/game.js
@@ -0,0 +1,11 @@
+//random images generate on page
+
+//listen for click event
+
+    //initial click:
+        // 1. the player's score is incremented 
+        // 2. images are shuffled in a different random order
+
+    //following clicks:
+
+        // 1. if user clicks on same image twice, the game is reset and score is back down to zero
