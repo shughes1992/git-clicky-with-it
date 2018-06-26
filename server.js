b8865a945521c7045847730cf718decd4247e90d
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2018-06-26 14:20:03 -0400

    6-26-18 folder Structuring

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
